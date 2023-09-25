34f26a936a57c457f3e4710543b2987a24602ccf
AhmedTrabelsi2000 <trabelsi.ahmed.pro@gmail.com>
2023-09-25 20:45:12 +0100

    deleting img  commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,17 @@
 import ContactForm from '@/components/ContactForm'
+import Image from 'next/image';
 require('dotenv').config();
 
 
 export default function Home() {
   return (
   <div className='p-4 max-w-3xl mx-auto'>
-    <img src="image/Broom.jpeg" alt="Description de l'image" />
+    <Image src="/image/Broom.jpeg" alt="Description de l'image" width={800} height={200} />
     <br/>
     <h1 className='text-2xl font-bold'>Make a reservation and Get your ticket !</h1>
     <br/>
     <p>Please fill the form below</p>
-    <p>                      And come dance with us!</p>
+    <p> And come dance with us!</p>
     <ContactForm/>
   </div>
   )
